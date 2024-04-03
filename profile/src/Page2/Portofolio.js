4c401b48e518fcd1a364a0bf3cb726c4fea590b9
Lalu Ardita Arip <ardita.arip18@gmail.com>
2024-04-03 12:23:09 +0800

    lalu
    install react dan tailwindcss

---
+++ profile/src/Page2/Portofolio.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Portofolio() {
+  return (
+    <div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Portofolio;
