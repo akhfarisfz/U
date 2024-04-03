e20f020808c5f8a2fa9309dec2491b4cc4a4a736
Lalu Ardita Arip <ardita.arip18@gmail.com>
2024-04-03 15:21:52 +0800

    lalu
    1

--- profile/src/Page2/Portofolio.js
+++ profile/src/Page2/Portofolio.js
@@ -1,13 +1,18 @@
 import React from "react";
+import imgPf from "../Asset/AssetPorto/aripPf1.png";
 import Portofolio1 from "./Portofolio1";
 import Portofolio2 from "./Portofolio2";
-
 function Portofolio() {
   return (
     <div>
       <div></div>
       <div>
-        <Portofolio1 desct="web adalah apliasi yang bla bla bal" judul="Web" />
+        <Portofolio1
+          judul="classification of monkey pox"
+          desct="Application to differentiate between common smallpox and monkey pox, by utilizing classification of existing data on common small pox and monkey pox."
+          src={imgPf}
+          alt="Image"
+        />
         <Portofolio2 />
       </div>
       <div></div>
--- profile/src/Page2/Portofolio1.js
+++ profile/src/Page2/Portofolio1.js
@@ -2,12 +2,14 @@ import React from "react";
 
 function Portofolio1(props) {
   return (
-    <div className="flex">
-      <div>
-        <h1>{props.judul}</h1>
-        <p>{props.desct}</p>
+    <div className="flex my-20 mx-20">
+      <div className="bg-slate-500">
+        <h1 className="font-bold text-5xl mt-14 mx-10">{props.judul}</h1>
+        <p className="text-xl mx-10">{props.desct}</p>
       </div>
-      <img src={props.src} alt={props.alt} />
+      <div>
+        <img className="rounded-lg size-96" src={props.src} alt={props.alt} />
+      </div>
     </div>
   );
 }
