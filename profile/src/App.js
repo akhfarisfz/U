f5b4ceae8c45f16af190abdc6e1c3ce6af48f646
Lalu Ardita Arip <ardita.arip18@gmail.com>
2024-04-04 11:28:36 +0800

    lalu

--- profile/src/App.js
+++ profile/src/App.js
@@ -1,9 +1,15 @@
 import bg from "../src/Asset/Bacground/bg1.jpg";
 import "./App.css";
+<<<<<<< Updated upstream
 import Footer from "./Page1/Footer";
 import Header from "./Page1/Header";
 import Index from "./Page1/Index";
 import Portofolio from "./Page2/Portofolio";
+=======
+import Header from "./Page1/Header";
+import Index from "./Page1/Index";
+// import Portofolio from "./Page2/Portofolio";
+>>>>>>> Stashed changes
 
 function App() {
   return (
@@ -13,8 +19,12 @@ function App() {
     >
       <Header />
       <Index />
+<<<<<<< Updated upstream
       <Portofolio />
       <Footer />
+=======
+      {/* <Portofolio /> */}
+>>>>>>> Stashed changes
     </div>
   );
 }
