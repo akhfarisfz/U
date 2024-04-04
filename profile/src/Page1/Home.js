22f16de9a210f0c9daf8afd7198f7bed65054cb9
Lalu Ardita Arip <ardita.arip18@gmail.com>
2024-04-04 16:56:33 +0800

    Perubahan pada app.js portofolio.js home.js iconbutton.js portopage1.js serta penambahan packgejason barua, create router dom

---
+++ profile/src/Page1/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import Index from "./Index";
+import PortoPage1 from "./PortoPage1";
+import Services from "./Services";
+function Home() {
+  return (
+    <div>
+      <Header />
+      <Index />
+      <Services />
+      <PortoPage1 />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
--- profile/src/Page1/Index.js
+++ profile/src/Page1/Index.js
@@ -3,15 +3,15 @@ import Ba1 from "../Asset/Bacground/bg2.png";
 
 function Index() {
   return (
-    <div className="flex my-14 mx-28">
-      <div className="w-3/5">
-        <h1 className="text-5xl font-bold">
-          Empower Your Business with UpZero
-        </h1>
+    <div className="flex my-14 mx-28 space-x-4">
+      <div className=" flex w-3/5 mt-16">
+        <div></div>
+        <h1 className="text-6xl font-bold">Empower Your Business with</h1>
+        <h1 className="text-7xl font-bold text-blue-400">UpZero</h1>
       </div>
       <div></div>
       <div className="w-2/5">
-        <img className="animate-spin  duration-300" src={Ba1} alt="Gambar" />
+        <img className="animate-pulse" src={Ba1} alt="Gambar" />
       </div>
     </div>
   );
