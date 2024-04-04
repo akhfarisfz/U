86a7d4225a41831b150fc00bf406a6e0de0d0602
Lalu Ardita Arip <ardita.arip18@gmail.com>
2024-04-04 11:27:19 +0800

    lalu
    5

--- profile/src/App.js
+++ profile/src/App.js
@@ -1,8 +1,8 @@
 import bg from "../src/Asset/Bacground/bg1.jpg";
 import "./App.css";
 import Footer from "./Page1/Footer";
-// import Header from "./Page1/Header";
-// import Index from "./Page1/Index";
+import Header from "./Page1/Header";
+import Index from "./Page1/Index";
 import Portofolio from "./Page2/Portofolio";
 
 function App() {
@@ -11,11 +11,10 @@ function App() {
       className="bg-fixed bg-cover bg-center "
       style={{ backgroundImage: `url(${bg})` }}
     >
-      {/* <Header />
-      <Index /> */}
+      <Header />
+      <Index />
       <Portofolio />
-      <Footer/>
-
+      <Footer />
     </div>
   );
 }
--- profile/src/Page1/Index.js
+++ profile/src/Page1/Index.js
@@ -2,13 +2,14 @@ import React from "react";
 import Ba1 from "../Asset/Bacground/bg2.png";
 function Index() {
   return (
-    <div className="flex my-14 mx-28">
-      <div className="">
-        <h1>Service is very good</h1>
-        <h1>For your digital problems</h1>
+    <div className="flex my-10 mx-28">
+      <div className="w-3/5">
+        <h1 className="text-7xl font-bold">
+          Empower Your Business with UpZero
+        </h1>
       </div>
-      <div className="">
-        <img className="w-1/2" src={Ba1} alt="Gambar" />
+      <div className="w-2/5">
+        <img className="animate-pulse " src={Ba1} alt="Gambar" />
       </div>
     </div>
   );
