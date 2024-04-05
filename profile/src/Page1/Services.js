90fe7c3c7e2a65b7f068e096f8f7544f8d184542
muhilhamfebr26 <muhilhamfebr2@gmail.com>
2024-04-05 13:05:31 +0700

    benerin Services

--- profile/src/Page1/Services.js
+++ profile/src/Page1/Services.js
@@ -4,7 +4,7 @@ import Pg1 from "../Asset/AssetPorto/gambar web app.png";
 function Services() {
   return (
     <div className="justify-center">
-      <div className="flex flex-wrap justify-center relative border-dotted border-2 border-indigo-600">
+      <div className="flex flex-wrap justify-center relative border-dotted border-2 border-indigo-600 h-48">
         <div className="flex justify-center pt-5 pb-16 relative">
           <h1 className="text-6xl text-slate-500 font-bold pr-5">Our</h1>
           <h1 className="text-6xl font-bold">Product</h1>
@@ -15,7 +15,7 @@ function Services() {
             alt="gambar"
             className="w-60 align-middle absolute bottom-2 left-2"
           />
-          <p className="absolute bottom-2 left-2 text-stone-50">Web App</p>
+          <p className="absolute bottom-2.5 left-3 text-stone-50">Web App</p>
         </div>
         <div className="w-60 relative">
           <img
@@ -23,7 +23,7 @@ function Services() {
             alt="gambar"
             className="w-60 align-middle absolute bottom-2 left-2"
           />
-          <p className="absolute bottom-2 left-2 text-stone-50">
+          <p className="absolute bottom-2.5 left-3 text-stone-50">
             Machine Learning
           </p>
         </div>
