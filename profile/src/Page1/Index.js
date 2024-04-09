ccc8127e904668a3110a6785e8c678b4592a49fe
akhfarisfz <akhfarifz6017@gmail.com>
2024-04-09 16:01:41 +0700

    Update some

--- profile/src/Page1/Index.js
+++ profile/src/Page1/Index.js
@@ -1,47 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-scroll";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import Ba1 from "../Asset/Bacground/bg2.png";
 
 function Index() {
+  const [isVisible, setIsVisible] = useState(false);
   const [kata] = useTypewriter({ words: ["UpZero"], loop: {} });
+
+  useEffect(() => {
+    setIsVisible(true);
+  }, []);
+
   return (
     <div
       id="cover"
-      class="flex flex-col md:flex-row justify-center items-center md:justify-between my-14 mx-4 md:mx-28 space-y-8 md:space-y-0 md:space-x-4"
+      className="flex flex-col md:flex-row justify-center items-center md:justify-between my-14 mx-4 md:mx-28 space-y-8 md:space-y-0 md:space-x-4 "
     >
-      <div class=" md:w-2/5 mt-8 md:mt-0 md:hidden ">
-        <img class="animate-pulse mx-auto" src={Ba1} alt="Gambar" />
+      <div className="md:w-2/5 mt-8 md:mt-0 md:hidden">
+        <img className="animate-pulse mx-auto" src={Ba1} alt="Gambar" />
       </div>
-      <div class="w-full md:w-3/5 mt-5 md:mt-0 md:ml-8">
-        <h1 class="text-4xl md:text-6xl font-bold">Empower Your Business</h1>
-        <h1 class="text-4xl md:text-6xl font-bold">
+      <div className=" w-full md:w-3/5 mt-5 md:mt-0 md:ml-8">
+        <h1 className={`text-4xl md:text-6xl font-bold ${isVisible ? "translate-y-0" : "translate-y-12 opacity-0"
+          } transition-all duration-700`}>Empower Your Business</h1>
+        <h1 className={`text-4xl md:text-6xl font-bold ${isVisible ? "translate-y-0" : "translate-y-12 opacity-0"
+          } transition-all duration-700 delay-150`}>
           with
-          <span class="text-5xl md:text-7xl font-bold ml-4 text-slate-400">
+          <span className="text-5xl md:text-7xl font-bold ml-4 text-slate-400">
             {" "}
             {kata}{" "}
           </span>
           <Cursor />
         </h1>
-        <h1 class="text-lg md:text-xl font-bold mt-5">
+        <h1 className={`text-lg md:text-xl font-bold mt-5   ${isVisible ? "translate-y-0" : "translate-y-12 opacity-0"
+          } transition-all duration-700 delay-200`}>
           Let's turn your brilliant idea into more advanced technology together
         </h1>
         <Link>
-          <button class="bg-slate-400 text-center mt-8 md:mt-20 px-7 py-2 font-bold rounded-md text-lg md:text-2xl">
+          <button className="bg-slate-400 text-center mt-8 md:mt-20 px-7 py-2 font-bold rounded-md text-lg md:text-2xl">
             <Link
               to="contactUs"
               smooth={true}
               duration={500}
               className="cursor-pointer"
             >
-              Contact US
+              Contact Us
             </Link>
           </button>
         </Link>
       </div>
 
-      <div class="w-full md:w-2/5 mt-8 md:mt-0 hidden md:block">
-        <img class="animate-pulse mx-auto" src={Ba1} alt="Gambar" />
+      <div className="w-full md:w-2/5 mt-8 md:mt-0 hidden md:block">
+        <img className="animate-pulse mx-auto" src={Ba1} alt="Gambar" />
       </div>
     </div>
   );
--- profile/src/Page1/Services.js
+++ profile/src/Page1/Services.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Pg2 from "../Asset/AssetPorto/gambar Machine learning app.jpg";
 import Pg1 from "../Asset/AssetPorto/gambar web app.png";
 
@@ -7,12 +7,12 @@ function ProductDescription({ imageSrc, altImage, title, description }) {
 
   return (
     <div
-      className="md:w-[500px] w-[450px] relative m-2"
+      className="md:w-full relative m-2 overflow-hidden transform transition-all duration-500"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
-      style={{ position: "relative" }}
+      style={{ minHeight: "250px" }}
     >
-      <div className="mx-20 sm:mx-5  " style={{ position: "relative" }}>
+      <div className="relative">
         <img
           src={imageSrc}
           alt={altImage}
@@ -27,7 +27,7 @@ function ProductDescription({ imageSrc, altImage, title, description }) {
         </p>
       </div>
       <div
-        className={`mx-20  sm:mx-5 rounded-lg absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center bg-black bg-opacity-50 ${
+        className={`text-sm sm:text-md lg:text-xl rounded-lg absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center bg-black bg-opacity-50 ${
           isHovered ? "opacity-100" : "opacity-0"
         } transition-opacity duration-300`}
       >
@@ -41,6 +41,28 @@ function ProductDescription({ imageSrc, altImage, title, description }) {
 }
 
 export default function Services() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const servicesSection = document.getElementById("Services");
+      if (servicesSection) {
+        const servicesSectionTop = servicesSection.offsetTop;
+        if (scrollY >= servicesSectionTop - window.innerHeight / 2) {
+          setIsVisible(true);
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Membersihkan event listener pada unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="justify-center" id="Services">
       <div className="flex flex-col mx-20 items-center relative h-auto">
@@ -53,7 +75,11 @@ export default function Services() {
           </h1>
           <h1 className="text-6xl font-bold">Product</h1>
         </div>
-        <div className="flex flex-col md:flex-row justify-center w-auto">
+        <div
+          className={`flex flex-col md:flex-row justify-center w-auto ${
+            isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-12"
+          } transition-all duration-700`}
+        >
           <ProductDescription
             imageSrc={Pg1}
             altImage="Web App"
